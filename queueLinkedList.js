3ef8af6801c0c44ae25056835c5cf00b4af6a9cb
kdlopresto <keith.lopresto@comcast.net>
2021-01-03 21:32:14 +0000

    add queue using linked list

---
+++ queueLinkedList.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  isEmpty() {
+    if (this.length === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  peek() {
+    if (!this.isEmpty()) {
+      return this.head;
+    } else {
+      return null;
+    }
+  }
+  
+  enque(value){
+    let newNode = new Node(value);
+    if (this.length === 0) {
+      this.tail = newNode;
+      this.head = this.tail;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  
+  dequeue(){
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    let tmpNode = this.head;
+    this.head = this.head.next;
+    tmpNode.next = null;
+    this.length--;
+
+    if (this.head === null) {
+      this.tail = this.head;
+    } 
+
+    return tmpNode;
+  }
+}
+
+const myQueue = new Queue();
+
+myQueue.enque("matt");
+myQueue.enque("prajwal");
+myQueue.enque("dan");
+myQueue.enque("venu");
+console.log(myQueue.peek());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue);
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+
+
+//Discord
+//Udemy
+//google
